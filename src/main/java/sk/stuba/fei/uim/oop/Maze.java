425fa3d0ae2aaa3fed910abcd93d3e51bb3a3479
simonkrupa <xkrupas@stuba.sk>
2021-04-18 21:46:19 +0200

    asi ide algo

--- src/main/java/sk/stuba/fei/uim/oop/Maze.java
+++ src/main/java/sk/stuba/fei/uim/oop/Maze.java
@@ -6,14 +6,24 @@ import java.util.Random;
 
 public class Maze {
     public ArrayList<MazePart> maze;
-    public int mazeWidthHeight = 11;
+    public int mazeWidthHeight = 13;
     public int mazeLength = mazeWidthHeight*mazeWidthHeight;
 
     public Maze() {
         maze = new ArrayList<>();
         for(int i = 0; i < mazeWidthHeight; i++){
             for(int j = 0; j < mazeWidthHeight; j++){
-                maze.add(new MazePart(mazeWidthHeight*i+j, i, j));
+                if(i==0 || i==12){
+                    maze.add(new MazePart((mazeWidthHeight)*i+j, i, j));
+                    maze.get((mazeWidthHeight)*i+j).pis = 'X';
+                }
+                else if(j==0 || j==12){
+                    maze.add(new MazePart((mazeWidthHeight)*i+j, i, j));
+                    maze.get((mazeWidthHeight)*i+j).pis = 'X';
+                }
+                else{
+                    maze.add(new MazePart((mazeWidthHeight)*i+j, i, j));
+                }
             }
         }
         /*
@@ -28,20 +38,22 @@ public class Maze {
     private void printMaze(){
         int j = 0;
         for(int i = 0; i < mazeLength; i++) {
-            if(i == j*11){
+            if(i == j*13){
                 System.out.println();
                 j++;
             }
             System.out.print(maze.get(i).pis);
         }
+        System.out.println();
     }
 
     private void generateMaze(){
-        MazePart startMazePart = maze.get(12);
+        MazePart startMazePart = maze.get(14);
         randomizedDFS(startMazePart);
     }
 
     private void randomizedDFS(MazePart mazePart){
+        printMaze();
         mazePart.setVisited(true);
         MazePart nextMazePart = randomUnvisitedNeighbour(mazePart);
         while(nextMazePart!=null){
@@ -55,20 +67,16 @@ public class Maze {
     private MazePart randomUnvisitedNeighbour(MazePart mazePart){
         ArrayList<MazePart> unvisitedNeighbours = new ArrayList<>();
         int currentMazePartPos = mazePart.getPosition();
-
-
-
-
-        if(maze.get(currentMazePartPos-1)!=null && !maze.get(currentMazePartPos - 1).isVisited()){
+        if(maze.get(currentMazePartPos-1).pis!='X' && !maze.get(currentMazePartPos - 1).isVisited()){
             unvisitedNeighbours.add(maze.get(currentMazePartPos-1));
         }
-        if(maze.get(currentMazePartPos + 1)!=null && !maze.get(currentMazePartPos + 1).isVisited()){
+        if(maze.get(currentMazePartPos + 1).pis!='X' && !maze.get(currentMazePartPos + 1).isVisited()){
             unvisitedNeighbours.add(maze.get(currentMazePartPos + 1));
         }
-        if(maze.get(currentMazePartPos - mazeWidthHeight)!=null && !maze.get(currentMazePartPos - mazeWidthHeight).isVisited()){
+        if(maze.get(currentMazePartPos - mazeWidthHeight).pis!='X' && !maze.get(currentMazePartPos - mazeWidthHeight).isVisited()){
             unvisitedNeighbours.add(maze.get(currentMazePartPos - mazeWidthHeight));
         }
-        if(maze.get(currentMazePartPos + mazeWidthHeight)!=null && !maze.get(currentMazePartPos + mazeWidthHeight).isVisited()){
+        if(maze.get(currentMazePartPos + mazeWidthHeight).pis!='X' && !maze.get(currentMazePartPos + mazeWidthHeight).isVisited()){
             unvisitedNeighbours.add(maze.get(currentMazePartPos + mazeWidthHeight));
         }
         if(unvisitedNeighbours.isEmpty()){
